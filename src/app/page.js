693c8300085b9e70fc422b6587c960783c93c531
Shahriear AL Amin <rion7213@gmail.com>
2025-11-12 15:29:22 +0600

    just started

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,217 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div>
+      <>
+        {/* Include this script tag or install `@tailwindplus/elements` via npm: */}
+        {/*  */}
+        <div className="bg-gray-900">
+          <header className="absolute inset-x-0 top-0 z-50">
+            <nav
+              aria-label="Global"
+              className="flex items-center justify-between p-6 lg:px-8"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              <div className="flex lg:flex-1">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                  <img
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                    alt=""
+                    className="h-8 w-auto"
+                  />
+                </a>
+              </div>
+              <div className="flex lg:hidden">
+                <button
+                  type="button"
+                  command="show-modal"
+                  commandfor="mobile-menu"
+                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    data-slot="icon"
+                    aria-hidden="true"
+                    className="size-6"
+                  >
+                    <path
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="hidden lg:flex lg:gap-x-12">
+                <a href="#" className="text-sm/6 font-semibold text-white">
+                  Product
+                </a>
+                <a href="#" className="text-sm/6 font-semibold text-white">
+                  Features
+                </a>
+                <a href="#" className="text-sm/6 font-semibold text-white">
+                  Marketplace
+                </a>
+                <a href="#" className="text-sm/6 font-semibold text-white">
+                  Company
+                </a>
+              </div>
+              <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <a href="#" className="text-sm/6 font-semibold text-white">
+                  Log in <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </nav>
+            <el-dialog>
+              <dialog
+                id="mobile-menu"
+                className="backdrop:bg-transparent lg:hidden"
+              >
+                <div tabIndex={0} className="fixed inset-0 focus:outline-none">
+                  <el-dialog-panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+                    <div className="flex items-center justify-between">
+                      <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        <img
+                          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                          alt=""
+                          className="h-8 w-auto"
+                        />
+                      </a>
+                      <button
+                        type="button"
+                        command="close"
+                        commandfor="mobile-menu"
+                        className="-m-2.5 rounded-md p-2.5 text-gray-200"
+                      >
+                        <span className="sr-only">Close menu</span>
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          data-slot="icon"
+                          aria-hidden="true"
+                          className="size-6"
+                        >
+                          <path
+                            d="M6 18 18 6M6 6l12 12"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                      <div className="-my-6 divide-y divide-white/10">
+                        <div className="space-y-2 py-6">
+                          <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                          >
+                            Product
+                          </a>
+                          <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                          >
+                            Features
+                          </a>
+                          <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                          >
+                            Marketplace
+                          </a>
+                          <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                          >
+                            Company
+                          </a>
+                        </div>
+                        <div className="py-6">
+                          <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
+                          >
+                            Log in
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </el-dialog-panel>
+                </div>
+              </dialog>
+            </el-dialog>
+          </header>
+          <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div
+              aria-hidden="true"
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+              />
+            </div>
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-30">
+              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
+                  Announcing our next round of funding.{" "}
+                  <a href="#" className="font-semibold text-indigo-400">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Read more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+              <div className="text-center">
+                <h1 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
+                  Data to enrich your online business
+                </h1>
+                <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+                  occaecat.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  >
+                    Get started
+                  </a>
+                  <a href="#" className="text-sm/6 font-semibold text-white">
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div
+              aria-hidden="true"
+              className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </>
     </div>
   );
-}
+};
+
+export default page;
