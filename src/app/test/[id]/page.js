ad2246a503060be058d4b2f1d402912004151518
Shahriear AL Amin <rion7213@gmail.com>
2025-11-30 17:04:31 +0600

    working..

--- src/app/test/[id]/page.js
+++ src/app/test/[id]/page.js
@@ -1,13 +1,23 @@
-const page = ({ params }) => {
-  const { id } = params;
-
+export default async function Page({ params }) {
+  const { id } = await params;
+  const data = await fetch("https://api.vercel.app/blog");
+  const posts = await data.json();
+  console.log(posts);
+  console.log({id});
   return (
-    <div className="h-[641px] bg-[black] text-white">
-      <ul>
-        <li className="text-[#ffffff]">The page id is : {id}</li>
-      </ul>
+    <div className=" h-[641px] bg-[black]">
+      {posts.map((item) => (
+          (item.id == id)?(
+        <div className=" h-[400px] w-[300px] bg-[#ffffff]">
+          <p>Product id : {item.id}</p>
+          <p>Product name : {item.author}</p>
+          <p>Product description : {item.category}</p>
+          <p>Product Prise {item.id}</p>
+        </div>           
+      ):
+      <p></p>
+    )
+    )}
     </div>
   );
-};
-
-export default page;
+}
--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,25 +1,27 @@
 import Link from "next/link";
 import React from "react";
 
-const page = () => {
-  const route = [
-    {
-      id: 1,
-      tittle: "Page 1",
-      Description: "This id page 1 description",
-    },
-    {
-      id: 2,
-      tittle: "Page 2",
-      Description: "This id page 2 description",
-    },
-  ];
+const page = async() => {
+    const data = await fetch('https://api.vercel.app/blog')
+  const posts = await data.json()
+  // const route = [
+  //   {
+  //     id: 1,
+  //     tittle: "Page 1",
+  //     Description: "This id page 1 description",
+  //   },
+  //   {
+  //     id: 2,
+  //     tittle: "Page 2",
+  //     Description: "This id page 2 description",
+  //   },
+  // ];
   return (
     <div className=" h-[641px] bg-[black] text-white">
       <ul>
-        {route.map((item) => (
+        {posts.map((item) => (
           <li key={item.id}>
-            <Link href={`/test/${item.id}`}>{item.tittle}</Link>
+            <Link href={`/test/${item.id}`}>item = {item.author}</Link>
           </li>
         ))}
       </ul>
